a2788d74f9af31548e5d9ce071788703c6ebc8dc
Yurii <sheremet.yu@gmail.com>
2019-07-16 20:29:01 +0300

    add proj

---
+++ Weather/weather.js
@@ -0,0 +1,32 @@
+var humidity, pressure, temperature, windSpeed, weatherSummary, button;
+
+function getElement(id) {
+    return document.getElementById(id);
+}
+
+window.onload = function() {
+    humidity = getElement('current-humidity');
+    pressure = getElement('current-pressure');
+    temperature = getElement('current-temperature');
+    windSpeed = getElement('current-wind-speed');
+    weatherSummary = getElement('weather-summary');
+    button = getElement('getCoordsPosition');
+    button.addEventListener('click', getPosition, false);
+}
+var 
+
+function getPosition() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var lat = position.coords.latitude,
+                lon = position.coords.longitude;
+            showWeather(lat, lon);   
+        });
+    } else {
+        alert('This browser does not support geolocation!');
+    }
+}
+
+function showWeather(lat, lon) {
+    console.log(lat, lon);
+}
